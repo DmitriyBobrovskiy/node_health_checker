3d605fe62de647a1ebe096b74aeb496012168919
Dmitriy Bobrovskiy <dmitriy.bobrovskiy@outlook.com>
2023-01-11 23:36:09 +0200

    make only one logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,48 @@
-export interface Logger {
-    info(message: string): void;
-    debug(message: string): void;
-    warn(message: string): void;
-    error(message: string): void;
-}
+import { Service } from "typedi";
+import winston, { createLogger, format, transports } from "winston";
 
+const {printf, combine, colorize, timestamp, errors} = format;
+
+@Service()
+export class Logger {
+
+    private logger: winston.Logger;
+
+    constructor() {
+        const devLogFormat = printf(
+            (log) =>
+                `${log.timestamp} ${log.level}: ${
+                    log.stack || typeof log.message === "object"
+                        ? JSON.stringify(log.message)
+                        : log.message
+                }`
+        );
+        this.logger = createLogger({
+                                       level: "debug",
+                                       format: combine(
+                                           colorize(),
+                                           timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
+                                           errors({stack: true}),
+                                           devLogFormat
+                                       ),
+                                       transports: [new transports.Console()],
+                                       exceptionHandlers: [new transports.Console()]
+                                   });
+    }
+
+    info(message: string): void {
+        this.logger.info(message);
+    }
+
+    debug(message: string): void {
+        this.logger.debug(message);
+    }
+
+    warn(message: string): void {
+        this.logger.warn(message);
+    }
+
+    error(message: string): void {
+        this.logger.error(message);
+    }
+}

7a454bf5589aeccf8d157776f74dfced79f1970f
Dmitriy Bobrovskiy <dmitriy.bobrovskiy@outlook.com>
2023-01-11 21:51:58 +0200

    implement data-provider-factory.ts

---
+++ src/services/data-provider-factory.ts
@@ -0,0 +1,18 @@
+import { NetworkType } from "../config/network-type";
+import { DataProvider } from "./data-provider";
+import { EthereumDataProvider } from "./data-providers/ethereum-data-provider";
+import { NearDataProvider } from "./data-providers/near-data-provider";
+
+export class DataProviderFactory {
+    // those arguments can be combined into single model
+    produce(networkType: NetworkType, nodeUrl: string, timeout: number, networkId?: string): DataProvider {
+        switch (networkType) {
+            case NetworkType.Ethereum:
+                return new EthereumDataProvider(nodeUrl, timeout);
+            case NetworkType.Near:
+                return new NearDataProvider(nodeUrl, timeout, <string>networkId);
+            default:
+                throw new Error(`Network type ${networkType} is not supported`);
+        }
+    }
+}
